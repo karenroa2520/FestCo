814fc4cf3d334787d576fb946b73a9894e2ec4dc
Heilly <verolopezq0516@gmail.com>
2025-10-22 10:20:06 -0500

    Estructura inicial

---
+++ src/test/java/com/senasoft/ferias/FeriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senasoft.ferias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
